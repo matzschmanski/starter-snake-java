af8efae8f908272f2f6e0f3051da4fb73dedfd2a
Matthias Geiger <matthias.geiger@arvato-scs.com>
2022-02-18 19:01:51 +0100

    curr

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -341,42 +341,124 @@ public class Snake {
             int[][] boardArray = new int[height][width];
             //not reachable:
             //self, other snakes, walls
-            StreamSupport.stream(board.get(SNAKES).spliterator(),false).flatMap(jsonNode -> StreamSupport.stream(jsonNode.get(BODY).spliterator(),false)).forEach(coordinate -> boardArray[coordinate.get(X).asInt()][coordinate.get(Y).asInt()] = -1);
 
-            // mark closest food as target
-            StreamSupport.stream(board.get(FOOD).spliterator(),false).flatMap(jsonNode -> StreamSupport.stream(jsonNode.get(BODY).spliterator(),false)).forEach(coordinate -> boardArray[coordinate.get(X).asInt()][coordinate.get(Y).asInt()] = -1);
+            for(int[] toBeFilled : boardArray)
+                Arrays.fill(toBeFilled, 3);
+
+            StreamSupport.stream(board.get(SNAKES).spliterator(),false).flatMap(jsonNode -> StreamSupport.stream(jsonNode.get(BODY).spliterator(),false)).forEach(coordinate -> boardArray[coordinate.get(X).asInt()][coordinate.get(Y).asInt()] = 0);
+
 
             return boardArray;
+        }
+
+        static boolean findPath(int M[][], int height, int width, Point destination)
+        {
+            // 1) Create BFS queue q
+            Queue<BFSElement> q = new LinkedList<>();
+
+            M[destination.x][destination.y] = 2;
+            // 2)scan the matrix
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    // if there exists a cell in the matrix such
+                    // that its value is 1 then push it to q
+                    if (M[i][j] == 1) {
+                        q.add(new BFSElement(i, j));
+                        break;
+                    }
+                }
+            }
+            // 3) run BFS algorithm with q.
+            while (q.size() != 0)
+            {
+                BFSElement x = q.peek();
+                q.remove();
+                int i = x.i;
+                int j = x.j;
+                // skipping cells which are not valid.
+                // if outside the matrix bounds
+                if (i < 0 || i >= height || j < 0 || j >= width)
+                    continue;
+                // if they are walls (value is 0).
+                if (M[i][j] == 0)
+                    continue;
+
+                // 3.1) if in the BFS algorithm process there was a
+                // vertex x=(i,j) such that M[i][j] is 2 stop and
+                // return true
+                if (M[i][j] == 2)
+                    return true;
+
+                // marking as wall upon successful visitation
+                M[i][j] = 0;
+
+                // pushing to queue u=(i,j+1),u=(i,j-1)
+                // u=(i+1,j),u=(i-1,j)
+                for (int k = -1; k <= 1; k += 2)
+                {
+                    q.add(new BFSElement(i + k, j));
+                    q.add(new BFSElement(i, j + k));
+                }
+            }
+
+            // BFS algorithm terminated without returning true
+            // then there was no element M[i][j] which is 2, then
+            // return false
+            return false;
+
+        }
+
+        static List<Point> getFoodOnBoard(JsonNode board ){
+            return StreamSupport.stream(board.get(SNAKES).spliterator(), false).flatMap(jsonNode -> StreamSupport.stream(jsonNode.get(BODY).spliterator(), false)).map(coordinate -> new Point(coordinate.get(X).asInt(), coordinate.get(Y).asInt())).collect(Collectors.toList());
+        }
+
+        static boolean isPath(Point startPosition, JsonNode moveRequest, int boardArray[][])
+        {
+            JsonNode board = moveRequest.get("board");
+            boardArray[startPosition.x][startPosition.y] = 1;
+
+            //get closest food
+            List<Point> foodOnBoard = getFoodOnBoard(board);
+// mark closest food as target
+//            .forEach(coordinate -> boardArray[coordinate.get(X).asInt()][coordinate.get(Y).asInt()] = 3);
+            JsonNode head = moveRequest.get(YOU).get(HEAD);
+            boardArray[head.get(X).asInt()][head.get(Y).asInt()] = 1;
+            // Mark reachable (from top left) nodes
+            // in first row and first column.
+            for (int i = 1; i < 5; i++)
+                if (boardArray[0][i] != -1)
+                    boardArray[0][i] = boardArray[0][i - 1];
+            for (int j = 1; j < 5; j++)
+                if (boardArray[j][0] != -1)
+                    boardArray[j][0] = boardArray[j - 1][0];
+
+            // Mark reachable nodes in
+            //  remaining matrix.
+            for (int i = 1; i < 5; i++)
+                for (int j = 1; j < 5; j++)
+                    if (boardArray[i][j] != -1)
+                        boardArray[i][j] = Math.max(boardArray[i][j - 1],
+                                boardArray[i - 1][j]);
+
+            // return yes if right
+            // bottom index is 1
+            return (boardArray[5 - 1][5 - 1] == 1);
+        }
+    }
+    static class BFSElement
+    {
+        int i, j;
+        BFSElement(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
         }
     }
 
 
-    static boolean isPath(int arr[][])
-    {
-        // set arr[0][0] = 1
-        arr[0][0] = 1;
 
-        // Mark reachable (from top left) nodes
-        // in first row and first column.
-        for (int i = 1; i < 5; i++)
-            if (arr[0][i] != -1)
-                arr[0][i] = arr[0][i - 1];
-        for (int j = 1; j < 5; j++)
-            if (arr[j][0] != -1)
-                arr[j][0] = arr[j - 1][0];
-
-        // Mark reachable nodes in
-        //  remaining matrix.
-        for (int i = 1; i < 5; i++)
-            for (int j = 1; j < 5; j++)
-                if (arr[i][j] != -1)
-                    arr[i][j] = Math.max(arr[i][j - 1],
-                            arr[i - 1][j]);
-
-        // return yes if right
-        // bottom index is 1
-        return (arr[5 - 1][5 - 1] == 1);
-    }
 
 
 
